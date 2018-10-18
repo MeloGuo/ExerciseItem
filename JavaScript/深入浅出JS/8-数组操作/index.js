af8c2833399700ed168c13b97022d14f15278802
MeloGuo <guoziliang199606@gmail.com>
2018-10-18 17:36:34 +0800

    8-数组操作：underscore解题

--- JavaScript/深入浅出JS/8-数组操作/index.js
+++ JavaScript/深入浅出JS/8-数组操作/index.js
@@ -88,3 +88,64 @@ Array.prototype.reduce = function (fn, initialValue = 0) {
   }
   return result
 }
+
+// TODO: 实现splice
+Array.prototype.splice = function () {
+
+}
+
+// 使用underscore.js
+import _ from 'underscore'
+
+var array = [
+  {id: 1, value: '1', parent_id: null},
+  {id: 2, value: '2', parent_id: null},
+  {id: 3, value: '1-1', parent_id: 1},
+  {id: 4, value: '1-2', parent_id: 1},
+  {id: 5, value: '2-1', parent_id: 2},
+  {id: 6, value: '2-2', parent_id: 2}
+]
+
+// const result = [
+//   {
+//       id:1,
+//       value: '1',
+//       children: [
+//           {id:3, value: '1-1', children: null},
+//           {id:4, value: '1-2', children: null},
+//       ]
+//   },
+//   {
+//       id:2,
+//       value: '2',
+//       children: [
+//           {id:5, value: '2-1', children: null},
+//           {id:5, value: '2-2', children: null},
+//       ]
+//   }
+// ]
+
+// 两次遍历法
+var convert = function (array) {
+  const parents = _.where(array, {parent_id: null})
+  const children = _.reject(array, function (object) {
+    return object.parent_id === null
+  })
+  _.each(parents, function (parent) {
+    parent.children = []
+    _.each(children, function (child) {
+      if (parent.id === child.parent_id) {
+        parent.children.push(child)
+        child.children = null
+        delete child.parent_id
+        delete parent.parent_id
+      }
+    })
+  })
+
+  return parents
+}
+
+console.log(convert(array))
+
+// TODO:单次遍历法
