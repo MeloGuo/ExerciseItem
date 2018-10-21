4f886b8904d6b4caabcb61ccaf1b1d4d20ac042e
MeloGuo <guoziliang199606@gmail.com>
2018-10-21 16:53:45 +0800

    ScriptOj：每日习题

--- JavaScript/ScriptOj/test.js
+++ JavaScript/ScriptOj/test.js
@@ -21,6 +21,18 @@ const fibonacci = (() => {
 console.log(fibonacci(10))
 
 /**
+ * #101 解析字符串 (正则表达式)
+ */
+const extractStr = (str) => {
+  const result = str.match(/:([^:\.])*?\./g) || [] // 如果没有匹配到会返回null，所以设置默认返回[]
+  return result.map((subStr) => {
+    return subStr.replace(/[:\.]/g, '')
+  })
+}
+// test
+console.log(extractStr('My name is:Jerry. My age is:12.'))
+
+/**
  * #99 safeGet
  */
 const safeGet = (data, path) => {
