10eef2f42b1d5965888350f161eead4d59c7da54
MeloGuo <guoziliang199606@gmail.com>
2018-10-21 20:46:40 +0800

    ScriptOj：每日习题

--- JavaScript/ScriptOj/test.js
+++ JavaScript/ScriptOj/test.js
@@ -33,6 +33,50 @@ const extractStr = (str) => {
 console.log(extractStr('My name is:Jerry. My age is:12.'))
 
 /**
+ * #100 把数字转换成中文
+ */
+const toChineseNum = (num) => {
+  if (num == 0) {
+    return '零'
+  }
+
+  const number = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九']
+  const unit = ['', '十', '百', '千', '万']
+  const resultStr = []
+  let len = 0
+  let lastNumNotZero = false
+
+  while (num) {
+    let n = num % 10
+    let u = len >= unit.length ? len % 5 + 1 : len % 5 // 这步确定基数很关键
+
+    // 添加基
+    if (n || (unit.lenth - 1 && len == unit.length - 1)) {
+      resultStr.unshift(unit[u])
+    }
+
+    // 添加数
+    if ((n || lastNumNotZero) && u != unit.length - 1) {
+      resultStr.unshift(number[n])
+    }
+
+    lastNumNotZero = !!n
+    len++
+    num = Math.floor(num / 10)
+  }
+
+  return resultStr.join('')
+}
+// test
+console.log(toChineseNum(12345678))
+console.log(toChineseNum(12345))
+console.log(toChineseNum(1234))
+console.log(toChineseNum(123))
+console.log(toChineseNum(12))
+console.log(toChineseNum(1))
+console.log(toChineseNum(0))
+
+/**
  * #99 safeGet
  */
 const safeGet = (data, path) => {
