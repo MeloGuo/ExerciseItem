26cf44ac5e8b338cb241bf112758e0d2c8bfd8b2
MeloGuo <guoziliang199606@gmail.com>
2018-11-17 22:48:44 +0800

    Python: 算法

--- Python/algorithm.py
+++ Python/algorithm.py
@@ -14,10 +14,13 @@ def binarySearch(list, item):
     else:
       high = mid - 1
   return None
-
 # test
 print(binarySearch([1, 3, 5, 7, 9], 3))
 print(binarySearch([1, 3, 5, 7, 9], 111))
+
+# recursion binary search
+def recursionBinarySearch(list, item):
+  pass  
 
 # selection sort
 def selectionSort(arr):
@@ -35,6 +38,34 @@ def selectionSort(arr):
     smallest = findSmallest(arr)
     newArr.append(arr.pop(smallest))
   return newArr
-
 # test
 print(selectionSort([5, 3, 7, 1]))
+
+# Fibonacci
+def fibonacci(parameter_list):
+  pass
+
+# Sum
+def sum(list):
+  if list == []:
+    return 0
+  return list[0] + sum(list[1:])
+# test
+print(sum([1, 2, 3]))
+
+# Count
+def count(list):
+  if list == []:
+    return 0
+  return 1 + count(list[1:])
+# test
+print(count([1, 2, 3, 4]))
+
+# Biggest Number
+def getMax(list):
+  if len(list) == 2:
+    return list[0] if list[0] > list[1] else list[1]
+  subMax = max(list[1:])
+  return list[0] if list[0] > subMax else subMax
+# test
+print(getMax([1, 2, 3, 4, 5]))
