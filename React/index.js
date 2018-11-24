2309d3c549a007906e4e2c33e33f6a014120cf19
MeloGuo <guoziliang199606@gmail.com>
2018-11-24 13:30:43 +0800

    前端组件化：优化DOM操作

--- React/index.js
+++ React/index.js
@@ -11,16 +11,25 @@ class LikeButton {
     }
   }
 
+  setState (state) {
+    const oldEl = this.el
+    this.state = state
+    this.el = this.render()
+    if (this.onStateChange) {
+      this.onStateChange(oldEl, this.el)
+    }
+  }
+
   changeLikeText () {
-    const likeText = this.el.querySelector('.like-text')
-    this.state.isLiked = !this.state.isLiked
-    likeText.innerHTML = this.state.isLiked ? '取消' : '点赞'
+    this.setState({
+      isLiked: !this.state.isLiked
+    })
   }
 
   render () {
     this.el = createDOMFromString(`
       <button class="like-btn">
-        <span class="like-text">点赞</span>
+        <span class="like-text">${this.state.isLiked ? '取消' : '点赞'}</span>
         <span>👍</span>
       </button>`)
     this.el.addEventListener('click', this.changeLikeText.bind(this), false)
@@ -29,7 +38,9 @@ class LikeButton {
 }
 
 const wrapper = document.querySelector('.wrapper')
-const likeButton1 = new LikeButton()
-wrapper.appendChild(likeButton1.render())
-const likeButton2 = new LikeButton()
-wrapper.appendChild(likeButton2.render())
+const likeButton = new LikeButton()
+wrapper.appendChild(likeButton.render())
+likeButton.onStateChange = (oldEl, newEl) => {
+  wrapper.insertBefore(newEl, oldEl)
+  wrapper.removeChild(oldEl)
+}
